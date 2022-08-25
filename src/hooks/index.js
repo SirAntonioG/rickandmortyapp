407105384a29aecea7ae9526bbee08db31564842
AntonGreg <antonio.pena@epn.edu.ec>
2022-08-25 17:04:06 -0500

    custom hook useCharacter

---
+++ src/hooks/index.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export const useCharacter = (name) => {
+  const [characters, setCharacters] = useState(null);
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character`)
+      .then((res) => {
+        if (res.data) {
+          setCharacters(res.data);
+        }
+      })
+      .catch((e) => {
+        setCharacters([]);
+      });
+  }, [name]);
+  return characters;
+};
